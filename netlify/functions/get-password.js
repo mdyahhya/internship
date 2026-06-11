70b89a81d7d6e46f5cb63a8992d351db086990ff
dominaltech <dominaltech@gmail.com>
2026-06-11 12:28:21 -0700

    add netlify functions

--- netlify/functions/get-password.js
+++ netlify/functions/get-password.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ password: process.env.PASS_MODULES || 'PASS_MODULES' }),
+    body: JSON.stringify({ password: '#FlutterJavascript' }),
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*'
